b02f400d0dec14d6162dcd067fdb9dd9b3043177
Leo <leooooo@bu.edu>
2015-05-22 00:35:08 -0400

    starting with just printing what i want to console
    getting there...

--- bbwm.py
+++ bbwm.py
@@ -10,8 +10,13 @@
 
 # globals (load from config file eventually)
 
-DEFAULT_PARTITION = "lol" # partitions will be a cool data type (probably a tuple) that represent the tiling "style"
-
+DEFAULT_PARTITION = "lol" # partitions will be a cool data type that represent the tiling "style"
+# specify the major partitioning/ratios and a default subpartitioning method
+# ex
+# [[(h, 3)] (v, 1)] # h = 0, v = 1
+# first window takes up whole screen
+# second window takes up rightmost third of screen ()
+# subsequent windows evenly split the rightmost third in height
 
 class Window():
 	"""
@@ -19,17 +24,57 @@ class Window():
 	attributes include - top positions for x & y, width, height, current workspace, current monitor, win32 handle
 	"""
 	# make the window object when a window is going to be tiled for the first time
-	def __init__(self,handle): 
+	def __init__(self,handle,x=-1,y=-1,w=-1,h=-1): 
 		self.handle = handle
-		self.x, self.y, self.w, self.h  = -1, -1, -1, -1 
+		self.x, self.y, self.w, self.h  = x, y, w, h
 
-	def set_pos(self, x, y): 
-		self.x, self.y = x, y
+	def __str__(self):
+		return self.handle + " @({0},{1}) w: {2} h: {3}".format(self.x, self.y, self.w, self.h)
 
-	def set_wh(self, w, h):
-		self.w, self.h = w, h
+	def resize(self, x, y, w, h): 
+		self.x, self.y, self.w, self.h  = x, y, w, h
 
-class Workplace():
+# reason for seperate containers and windows is in case where you want to have one window take up the space you usually would've gotten if you reflowed once more
+# aka in standard ex if you had 3 windows, 1 on 2/3rd left, 2 & 3 each taking up half of the remaining third, if you wanted 3 to take up 2/3rd of the third
+
+class Container():
+	"""
+	each container contains windows or subcontainers
+	also specifies how it will be partitioned
+	"""
+
+	def __init__(self,parent,window,x,y,w,h,hv=0,r=1): 
+		# vh - default split for new containers (0 for horizontal 1 for vertical, r - ratio of split
+		# hv is either 0 or 1, r is any positive rational number :^) 
+		self.parent = parent # the workspace
+		self.contents = []
+		self.x, self.y, self.w, self.h = x, y, w, h
+		self.hv, self.r = hv, r
+
+	def resize(self, x, y, w, h): 
+		self.x, self.y, self.w, self.h  = x, y, w, h
+
+	def get_dims(self):
+		return self.x, self.y, self.w, self.h
+
+	def add_sub(self,window,hv = None): # hv allows to partition non-default way if passed
+		if hv is None: hv = self.hv
+		if self.r == 1:
+			new_r = len(self.contents)+1
+		else:
+			new_r = self.r
+		if hv == 0:
+			new_w, new_h = self.w - self.w // new_r, self.h
+		else:
+			new_w, new_h = self.w, self.h - self.h // new_r
+		self.resize(self.x,self.y,new_w,new_h)
+		new_sub = Container(self.parent,self.x+new_w*(1-hv),self.y+new_h*hv,self.w-new_w*(1-hv),self.h-new_h*hv)
+		#fug
+		self.parent.add_container(new_sub)
+
+
+
+class Workspace():
 	"""
 	used to represent a virtual screen
 	contains windows in set positions that can be moved around
@@ -37,22 +82,37 @@ class Workplace():
 	"""
 	def __init__(self,n,W,H):
 		self.n = n
-		self.wins = [] # first version will do everything with arrays because they are ez, will see about more efficient structures later
+		self.containers = [] # first version will do everything with arrays because they are ez, will see about more efficient structures later
 		self.W, self.H = W, H
 		self.p_style = DEFAULT_PARTITION
+		self.main_container = Container(self,None,0,0,W,H)
 
 	def __str__(self):
 		# print out a cute picture
-		print("[]")
+		tempa = [[0 for x in range(self.W)] for x in range(self.H)] 
+		tor = "Workspace #{0} W: {1} H: {2}".format(self.n,self.W,self.H) + "\n"
+		for j in range(self.H):
+			for i in range(self.W):
+				tor = tor + str(tempa[j][i])
+			tor = tor + "\n"
+		return tor
 
-	def add_window(self,window,where=-1): # adds a window and tiles it, with the possiblity of tiling in new creative way : )
-		self.wins.append(window)
-		self.reflow()
+	def add_container(self,container,window,where=-1): # adds a window and tiles it, with the possiblity of tiling in new creative way : )
+		self.containers.append(container)
+		self.reflow(window,where)
 
-	def reflow():
-		# compute where everything should go based on the partitioning
-		# first will do default ez pz ex
-		nw = len(wins)
-		for w in self.wins:
-			w.set_wh(int(self.W/nw),self.H)
-			w.set_pos() # here is where i realized i would rather do this with recursion and stopped for the day : )
+	def reflow(self,window,where):
+		self.containers[where].add_sub(window)
+
+
+
+
+
+if __name__=='__main__':
+	testwin = Window("lol",1,2,3,4)
+	#print(testwin)
+	testworkspace = Workspace(1,12,6)
+	print(testworkspace)
+	testcontainer = Container(testworkspace,testwin,0,0,6,3)
+	print(testcontainer.get_dims())
+	testworkspace.add_container(testcontainer,testwin)
