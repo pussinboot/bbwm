046a4b821c6c16461b8994ef526deea662912588
pussinboots <pussinboots@cock.li>
2017-09-17 22:37:17 -0400

    pretty

--- bbwm/drawing.py
+++ bbwm/drawing.py
@@ -1,57 +1,5 @@
 import tkinter as tk
 
-
-class TestDraw:
-    def __init__(self, root, base_dims, config):
-        WIDTH, HEIGHT = base_dims.w, base_dims.h
-
-        self.c = config
-
-        self.root = root
-        self.root.title('bbwm test')
-
-        self.canvas = tk.Canvas(self.root, width=WIDTH, height=HEIGHT, bg="#444")
-        self.canvas.pack()
-
-        self.cursor = None
-
-        self.root.protocol('WM_DELETE_WINDOW', self.kill)
-
-    def get_bbox(self, dims):
-        x, y, w, h = dims.x, dims.y, dims.w, dims.h
-        lx, rx = x + self.c.INNER_SPACING_X, x + w - self.c.INNER_SPACING_X
-        ty, by = y + self.c.INNER_SPACING_Y, y + h - self.c.INNER_SPACING_Y
-        return lx, ty, rx, by
-
-    def draw_border(self, dims, highlight=False):
-        if highlight:
-            outline = self.c.BORDER_HIGHLIGHT_COLOR
-        else:
-            outline = self.c.BORDER_COLOR
-
-        w = max(min(self.c.INNER_SPACING_X, self.c.INNER_SPACING_Y) - 2, 2)
-        self.canvas.create_rectangle(self.get_bbox(dims), outline=outline, width=w)
-
-    def draw_fill(self, dims):
-        self.canvas.create_rectangle(self.get_bbox(dims), fill=self.c.FAKE_WIN_COLOR)
-
-    def draw_cursor(self, dims):
-        cx, cy = dims.midpoint()
-        r = self.c.CURSOR_SIZE
-        if self.cursor is None:
-            color = self.c.CURSOR_COLOR
-            self.cursor = self.canvas.create_oval(cx - r, cy - r, cx + r, cy + r, fill=color)
-        else:
-            self.canvas.coords(self.cursor, cx - r, cy - r, cx + r, cy + r)
-
-    def clear(self):
-        self.canvas.delete('all')
-        self.cursor = None
-
-    def kill(self):
-        self.root.destroy()
-
-
 class BBDraw:
     # i guess this is per monitor -.-
     def __init__(self, root, base_dims, c):
@@ -60,13 +8,16 @@ class BBDraw:
         self.root = root
         self.root.title('bbwm')
         self.root.attributes('-alpha', self.c.DEFAULT_OPACITY)
+        root.wm_attributes("-topmost", True)
+        root.wm_attributes("-transparentcolor", self.c.TRANSPARENT_COLOR)
+        root.overrideredirect(True)
 
         self.width, self.height = base_dims.w, base_dims.h
         w, h = self.width + 2 * c.OFF_SCREEN, self.height + 2 * c.OFF_SCREEN
         x, y = -c.OFF_SCREEN, -c.OFF_SCREEN
         self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-        self.canvas = tk.Canvas(root, width=w, height=h, bg="#0DEAD0")
+        self.canvas = tk.Canvas(root, width=w, height=h, bg=self.c.TRANSPARENT_COLOR)
         self.canvas.pack()
 
         self._draw_job = None
