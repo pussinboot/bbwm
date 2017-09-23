b10fbb866206c0a0a23460dc3234d2a677a6de67
pussinboots <pussinboots@cock.li>
2017-09-22 21:36:16 -0400

    separate tiling

--- bbwm/core.py
+++ bbwm/core.py
@@ -1,5 +1,9 @@
 from collections import namedtuple
-import abc
+
+try:
+    from tiling import DefaultTilingScheme, HorizontalTilingScheme
+except:
+    from .tiling import DefaultTilingScheme, HorizontalTilingScheme
 
 
 # geometry
@@ -106,14 +110,16 @@ class Workspace:
         self.base_dims = base_dims
 
         if tile_scheme is None:
-            tile_scheme = HorizontalTilingScheme()
+            tile_scheme = DefaultTilingScheme()
+            # tile_scheme = HorizontalTilingScheme()
         self.tile_scheme = tile_scheme
 
         if first_child is None:
-            first_child = Partition(None, self.base_dims)
+            first_child = Partition(None, self.base_dims, ts=self.tile_scheme)
         else:
             first_child.parent = self
             first_child.dims = self.base_dims
+            first_child.assoc_ts = self.tile_scheme
 
         self.children = [first_child]
         self.cur_part = first_child
@@ -379,7 +385,7 @@ class Workspace:
 
 
 class Partition:
-    def __init__(self, parent, dims, index=0, win=None):
+    def __init__(self, parent, dims, index=0, win=None, ts=None):
         self.parent = parent
         self.children = []
 
@@ -389,6 +395,7 @@ class Partition:
         self.window = win
         if self.window is not None:
             self.window.part = self
+        self.assoc_ts = ts  # associated tile-scheme
 
     @property
     def is_empty(self):
@@ -491,83 +498,6 @@ class Partition:
 
     def split_v(self, r=0.5, new_win=None):
         return self._split('v', r, new_win)
-
-
-# tiling logic
-class TileScheme:
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self):
-        pass
-
-    @abc.abstractmethod
-    def tile(self, part, new_win=None):
-        return
-
-    @abc.abstractmethod
-    def manual_tile(self, part, new_win=None):
-        return
-
-    @abc.abstractmethod
-    def untile(self, part):
-        return
-
-
-class DefaultTilingScheme(TileScheme):
-    def __init__(self):
-        super().__init__()
-        self.tile_count = 0
-
-    def tile(self, part, new_win=None):
-        if new_win is not None:
-            if new_win.part is not None:
-                # don't retile
-                return
-            elif part.window is None:
-                # first add to current partition if it's empty
-                part.window = new_win
-                new_win.part = part
-                return
-        if self.tile_count == 0:
-            np = part.split_h(0.67, new_win)
-        else:
-            np = part.split_v(0.5, new_win)
-        if np is not None:
-            self.tile_count += 1
-            return np
-
-    def manual_tile(self, part, new_win=None):
-        if part is not None:
-            self.tile_count += 1
-
-    def untile(self, part, new_win=None):
-        self.tile_count = max(0, self.tile_count - 1)
-
-
-class HorizontalTilingScheme(TileScheme):
-    def __init__(self):
-        super().__init__()
-
-    def tile(self, part, new_win=None):
-        if new_win is not None:
-            if new_win.part is not None:
-                # don't retile
-                return
-            elif part.window is None:
-                # first add to current partition if it's empty
-                part.window = new_win
-                new_win.part = part
-                return
-        return part._multi_split('h', new_win)
-
-    def manual_tile(self, part, new_win=None):
-        pass
-
-    def untile(self, part, new_win=None):
-        if part.parent is not None:
-            pps = part.parent.split
-            if pps is not None:
-                part.parent.split = Split(pps.d, max(1, pps.r - 1), pps.t)
 
 
 # configuration
